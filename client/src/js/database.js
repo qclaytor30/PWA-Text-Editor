46de6a383df749bed7d665d33e823553293eb664
qclaytor30 <qclaytor30@gmail.com>
2022-12-20 04:04:09 -0500

    add js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,5 +1,5 @@
-import { openDB } from 'idb';
-
+import {request} from 'express';
+import {openDB} from 'idb';
 const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
@@ -8,14 +8,27 @@ const initdb = async () =>
         return;
       }
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      console.log('jate database created')
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-
-initdb();
+export const postDb = async (value) => {
+  console.log('posting to the jateDB');
+  
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const objStore = tx.objectStore('jate');
+  const req = objStore.add({ value: value })
+  const result = await req;
+  console.log('data saved to the jateDB', res);
+};
+export const putDb = async (id, value) => {
+  console.log('PUT request to update the jateDB');
+  
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const objStore = tx.objectStore('jate');
+  const req = objStore.put({ id: id, value: value })
+  const result = await req;
+  console.log('data saved to the jateDB', res);
+};
